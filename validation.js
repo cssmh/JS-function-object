40f1ce4ad24ba081a02c41a68f3c972aba9f3bbc
Momen Hossain <139069345+Git-Momen@users.noreply.github.com>
2023-10-04 12:44:05 +0600

    Update validation.js

--- validation.js
+++ validation.js
@@ -1,22 +1,22 @@
-// function sum(a,b){
-//     if(typeof a !== "number" || typeof b !== "number"){
-//         return "duita number de shala"
-//     }
-//     return(a+b);
-// }
-// console.log(sum('4',5));
+function sum(a,b){
+    if(typeof a !== "number" || typeof b !== "number"){
+        return "duita number de shala"
+    }
+    return(a+b);
+}
+console.log(sum('4',5));
 
-// function findSum(arr){
+function findSum(arr){
 
-//     if(!Array.isArray(arr)){
-//         return "please provide me an Array"
-//     }
+    if(!Array.isArray(arr)){
+        return "please provide me an Array"
+    }
 
-//     let sum = 0;
-//     for(let i of arr){
-//         sum+=i
-//     }
-//     return sum;
-// }
-// const x = [1,2,3,4,5];
-// console.log(findSum("dfalfjdalfjalf"))
+    let sum = 0;
+    for(let i of arr){
+        sum+=i
+    }
+    return sum;
+}
+const x = [1,2,3,4,5];
+console.log(findSum("dfalfjdalfjalf"))
