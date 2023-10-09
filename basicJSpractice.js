d31519b72ab1e250f8d81e97e7f80f09f4a96ef3
Momen Hossain <139069345+Git-Momen@users.noreply.github.com>
2023-10-09 22:25:17 +0600

    Create basicJSpractice.js

---
+++ basicJSpractice.js
@@ -0,0 +1,121 @@
+// const one = 34;
+// const two = 34;
+// const tw2 = 34;
+// if(one != two && two == tw2){
+//     console.log("kam hoice");
+// }
+// else{
+//     console.log("tui sesh");
+// }
+
+// let i = 5;
+// while(i < 19){
+//     i+=2;
+//     const ok = i;
+//     console.log(ok);
+// }
+
+// let array = [1,3,45,546,4534,32,2];
+// console.log(array.length);
+// array[4] = 8;
+// array.push(12, 21);
+// array.shift()
+// console.log(array);
+
+// let array = [1, 4, 2, 21, 3];
+// for (let item of array) {
+//   console.log(item);
+// }
+
+// let array = [1,4,2,21,3];
+// for(let i = 0; i<= array.length; i++){
+//     console.log(array[i]);
+// }
+
+
+// let number = [23,97,53,42,24,55,66,883,34];
+// for(let i =0; i< number.length; i++){
+//     if(number[i]>80){
+//         console.log("boro",number[i]);
+//     }
+//     else{
+//         console.log("Eida borona",number[i]);
+//     }
+// }
+
+
+// function sum(a,b,c){
+//     const gun = a*b*c;
+//     return gun;
+// }
+// const ac = 2;
+// const bc = 4;
+// const cc = 3;
+// const result = sum(ac,bc,cc);
+// console.log(result);
+
+
+// let person = {
+//     name: "jon de",
+//     age: 30,
+//     city: "tangail"
+//   };
+  
+//   person.age = 35;
+  
+//   console.log(person);
+
+
+// function feetToInch(feet){
+//   const res = feet * 12;
+//   return res;
+// }
+// console.log(feetToInch(4));
+
+
+// function centimeterToMeter(centimeter){
+//   const res = centimeter * 0.01;
+//   return res;
+// }
+// console.log(centimeterToMeter(100));
+
+// function paperRequirements(c1, c2, c3){
+//   const first = c1 * 100;
+//   const first1 = c2 * 200;
+//   const first2 = c3 * 300;
+//   const total = first + first1 + first2;
+//   return total;
+// }
+// const result = paperRequirements(5,3,2);
+// console.log(result);
+
+// function bestFriend(array){
+//   let largest = array[0];
+//   for(i = 0; i < array.length; i++){
+//     if(largest.length < array[i].length){
+//       largest = array[i];
+//     }
+//   }
+//   return largest;
+// }
+// const friendsAre = ["kana","kamla vanu dia", "komin", "beyaitta", "tomar nani"];
+// const result = bestFriend(friendsAre);
+// console.log(result);
+
+// function processArray(arr){
+//   const positiveNumbers = [];
+//   for(let i = 0; i < arr.length; i++){
+//     if(arr[i] < 0){
+//       break;
+//     }
+//     else{
+//       positiveNumbers.push(arr[i])
+//     }
+//   }
+//   return positiveNumbers;
+// }
+// const inputArray = [1, 2, 3, 4, 5, 6, 7, -1, 8];
+// const result = processArray(inputArray);
+// console.log(result);
+
+// console.log(Math.round(5.95), Math.round(5.3), Math.round(5.05));
